23a9c969b3b92c373eb331df3ecb43ef3b501f7f
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2018-06-09 21:32:23 +0200

    feat: add customer dummy data

---
+++ dummy_data.py
@@ -0,0 +1,59 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from DBmodel import (Base,
+                     Customer
+                     )
+
+
+engine = create_engine('sqlite:///transportation.db')
+Base.metadata.bind = engine
+DBsession = sessionmaker(bind=engine)
+session = DBsession()
+
+# Customer Data
+cumstomer1 = Customer(first_name="Hossam",
+                      last_name="Khaled",
+                      email="hossam@gmail.com",
+                      password="hossam123",
+                      identity="12345678912365",
+                      rate=0.0,
+                      phone="12345698742",
+                      )
+session.add(cumstomer1)
+session.commit()
+
+cumstomer2 = Customer(first_name="Omar",
+                      last_name="Ramzi",
+                      email="omar@gmail.com",
+                      password="omar123",
+                      identity="12345678912365",
+                      rate=0.0,
+                      phone="12345698742",
+                      )
+session.add(cumstomer2)
+session.commit()
+
+cumstomer3 = Customer(first_name="Sayed",
+                      last_name="Ashref",
+                      email="sayed@gmail.com",
+                      password="sayed123",
+                      identity="12345678912365",
+                      rate=0.0,
+                      phone="12345698742",
+                      )
+session.add(cumstomer3)
+session.commit()
+
+cumstomer4 = Customer(first_name="Khaled",
+                      last_name="Ibrahim",
+                      email="khaled@gmail.com",
+                      password="khaled123",
+                      identity="12345678912365",
+                      rate=0.0,
+                      phone="12345698742",
+                      )
+session.add(cumstomer4)
+session.commit()
+
+
+print "dummy data successfully added"
